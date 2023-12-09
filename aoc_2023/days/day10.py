dad45c9e075e779f3a04e95238787e54df06b0e2
CGL <charles.giry.laterriere@gmail.com>
2023-12-09 07:01:04 +0100

    prepare day 10

---
+++ aoc_2023/days/day10.py
@@ -0,0 +1,20 @@
+"""
+aoc 2023 day 10
+https://adventofcode.com/2023/day/10
+"""
+with open('data/day10_data.txt', 'r') as file:
+    lines = file.read().splitlines()
+
+
+def d10p1():
+    """
+
+    """
+    pass
+
+
+def d10p2():
+    """
+
+    """
+    pass
