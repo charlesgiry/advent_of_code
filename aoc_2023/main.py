7eb993e74f25b977043552bb3e0d7de2d0339fc9
CGL <charles.giry.laterriere@gmail.com>
2023-12-11 08:12:59 +0100

    uncommented the main

--- aoc_2023/main.py
+++ aoc_2023/main.py
@@ -1,15 +1,15 @@
 from time import perf_counter
 
-# from days.day01 import d1p1, d1p2
-# from days.day02 import d2p1, d2p2
-# from days.day03 import d3p1, d3p2
-# from days.day04 import d4p1, d4p2
-# from days.day05 import d5p1, d5p2
-# from days.day06 import d6p1, d6p2
-# from days.day07 import d7p1, d7p2
-# from days.day08 import d8p1, d8p2
-# from days.day09 import d9p1, d9p2
-# from days.day10 import d10p1, d10p2
+from days.day01 import d1p1, d1p2
+from days.day02 import d2p1, d2p2
+from days.day03 import d3p1, d3p2
+from days.day04 import d4p1, d4p2
+from days.day05 import d5p1, d5p2
+from days.day06 import d6p1, d6p2
+from days.day07 import d7p1, d7p2
+from days.day08 import d8p1, d8p2
+from days.day09 import d9p1, d9p2
+from days.day10 import d10p1, d10p2
 from days.day11 import d11p1, d11p2
 
 def measure(func):
@@ -30,44 +30,44 @@ def main():
     """
     main
     """
-    # measure(d1p1)
-    # measure(d1p2)
-    # print('')
-    #
-    # measure(d2p1)
-    # measure(d2p2)
-    # print('')
-    #
-    # measure(d3p1)
-    # measure(d3p2)
-    # print('')
-    #
-    # measure(d4p1)
-    # measure(d4p2)
-    # print('')
-    #
-    # measure(d5p1)
-    # measure(d5p2)
-    # print('')
-    #
-    # measure(d6p1)
-    # measure(d6p2)
-    # print('')
-    #
-    # measure(d7p1)
-    # measure(d7p2)
-    # print('')
-    #
-    # measure(d8p1)
-    # measure(d8p2)
-    # print('')
-    #
-    # measure(d9p1)
-    # measure(d9p2)
-    # print('')
-    #
-    # measure(d10p1)
-    # # measure(d10p2)    # takes about 10 secs so skipping it unless i really want to calculate it
+    measure(d1p1)
+    measure(d1p2)
+    print('')
+
+    measure(d2p1)
+    measure(d2p2)
+    print('')
+
+    measure(d3p1)
+    measure(d3p2)
+    print('')
+
+    measure(d4p1)
+    measure(d4p2)
+    print('')
+
+    measure(d5p1)
+    measure(d5p2)
+    print('')
+
+    measure(d6p1)
+    measure(d6p2)
+    print('')
+
+    measure(d7p1)
+    measure(d7p2)
+    print('')
+
+    measure(d8p1)
+    measure(d8p2)
+    print('')
+
+    measure(d9p1)
+    measure(d9p2)
+    print('')
+
+    measure(d10p1)
+    # measure(d10p2)    # takes about 10 secs so skipping it unless i really want to calculate it
     print('')
 
     measure(d11p1)
