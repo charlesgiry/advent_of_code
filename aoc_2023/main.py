0cfebe9828f2d02f11bd0b8e8bd9785d560f23aa
CGL <charles.giry.laterriere@gmail.com>
2023-12-13 05:58:34 +0100

    refactor to make the measure function more generic

--- aoc_2023/main.py
+++ aoc_2023/main.py
@@ -1,4 +1,5 @@
 from time import perf_counter
+from traceback import print_exc
 
 from days.day01 import d1p1, d1p2
 from days.day02 import d2p1, d2p2
@@ -15,24 +16,40 @@ from days.day12 import d12p1, d12p2
 from days.day13 import d13p1, d13p2
 
 
-def measure(func):
+def measure(func, *args, **kwargs):
     """
     measure the performances of a method execution using time.perf_counter
     """
     start = perf_counter()
-    result = func()
-    perf = perf_counter() - start
-    perf = "{:.4f}".format(perf)
-    if result:
-        print(f'{func.__name__}: {result} - took {perf}s')
+    print_str = f'{func.__name__}' \
+                f'({", ".join(str(arg) for arg in args)}' \
+                f'{", " if args and kwargs else ""}' \
+                f'{", ".join(f"{key}={value}" for key, value in kwargs.items())})'
+    try:
+        result = func(*args, **kwargs)
+
+    except:
+        perf = perf_counter() - start
+        perf = "{:.6f}".format(perf)
+        print_str = f'{print_str} - encountered an exception after {perf}s'
+        print_exc()
+
     else:
-        print(f'{func.__name__} - took {perf}s')
+        perf = perf_counter() - start
+        perf = "{:.6f}".format(perf)
+        if result:
+            print_str = f'{print_str}: {result} - took {perf}s'
+        else:
+            print_str = f'{print_str} - took {perf}s'
+
+    print(print_str)
 
 
 def main():
     """
     main
     """
+    print('')
     measure(d1p1)
     measure(d1p2)
     print('')
