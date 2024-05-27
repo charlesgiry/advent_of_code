b98723deda14daab5837352e81b694419d5f4c44
Charles Giry-Laterriere <charles.giry-laterriere@web-atrio.com>
2024-05-27 15:44:58 +0200

    changed imports to use absolute instead of relative imports

--- y2015/days/__init__.py
+++ y2015/days/__init__.py
@@ -1,14 +1,15 @@
 """
 
 """
-from .day01 import d1parse, d1p1, d1p2, d1p1_old
-from .day02 import d2parse, d2p1, d2p2
-from .day03 import d3parse, d3p1, d3p2
-from .day04 import d4parse, d4p1, d4p2
-from .day05 import d5p1, d5p2, d5p2
-from .day06 import d6parse, d6p1, d6p2, d6p1_old, d6p2_old
-from .day07 import d7parse, d7p1, d7p2
-from .day08 import d8p1, d8p2
-from .day09 import d9parse, d9p1, d9p2
-from .day10 import d10parse, d10p1, d10p2
-from .day11 import d11parse, d11p1, d11p2
+from y2015.days.day01 import d1parse, d1p1, d1p2, d1p1_old
+from y2015.days.day02 import d2parse, d2p1, d2p2
+from y2015.days.day03 import d3parse, d3p1, d3p2
+from y2015.days.day04 import d4parse, d4p1, d4p2
+from y2015.days.day05 import d5p1, d5p2, d5p2
+from y2015.days.day06 import d6parse, d6p1, d6p2, d6p1_old, d6p2_old
+from y2015.days.day07 import d7parse, d7p1, d7p2
+from y2015.days.day08 import d8p1, d8p2
+from y2015.days.day09 import d9parse, d9p1, d9p2
+from y2015.days.day10 import d10parse, d10p1, d10p2
+from y2015.days.day11 import d11parse, d11p1, d11p2
+from y2015.days.day12 import d12parse, d12p1, d12p2
--- y2023/days/__init__.py
+++ y2023/days/__init__.py
@@ -1,13 +1,13 @@
 """
 y2023 code for each day
 """
-from .day01 import d1p1, d1p2
-from .day02 import d2p1, d2p2, d2parse
-from .day03 import d3p1, d3p2, d3parse
-from .day04 import d4p1, d4p2, d4p2_old
-from .day05 import d5p1, d5p2, d5parse
-from .day06 import d6p1, d6p2, d6p2_old, d6parse
-from .day07 import d7p1, d7p2, d7parse
-from .day08 import d8p1, d8p2, d8parse
-from .day09 import d9p1, d9p2, d9parse
-from .day10 import d10p1, d10p2, d10parse
+from y2023.days.day01 import d1p1, d1p2
+from y2023.days.day02 import d2p1, d2p2, d2parse
+from y2023.days.day03 import d3p1, d3p2, d3parse
+from y2023.days.day04 import d4p1, d4p2, d4p2_old
+from y2023.days.day05 import d5p1, d5p2, d5parse
+from y2023.days.day06 import d6p1, d6p2, d6p2_old, d6parse
+from y2023.days.day07 import d7p1, d7p2, d7parse
+from y2023.days.day08 import d8p1, d8p2, d8parse
+from y2023.days.day09 import d9p1, d9p2, d9parse
+from y2023.days.day10 import d10p1, d10p2, d10parse
