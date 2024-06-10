d27306b880af402282ae49aa3c087acba844ae7b
Charles Giry-Laterriere <charles.giry-laterriere@web-atrio.com>
2024-06-10 17:47:04 +0200

    aoc 2015 day 20

--- y2015/days/__init__.py
+++ y2015/days/__init__.py
@@ -20,3 +20,4 @@ from y2015.days.day16 import d16parse, d16p1, d16p2
 from y2015.days.day17 import d17parse, d17p1, d17p2
 from y2015.days.day18 import d18parse, d18p1, d18p2, d18p1_old
 from y2015.days.day19 import d19parse, d19p1, d19p2
+from y2015.days.day20 import d20parse, d20p1, d20p2
