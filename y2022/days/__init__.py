ed3bbc424a9ce445cf8ea4e641a7f73f74dc1f43
CGL <charles.giry.laterriere@gmail.com>
2024-12-03 18:50:04 +0100

    init y2022 day02

--- y2022/days/__init__.py
+++ y2022/days/__init__.py
@@ -2,3 +2,4 @@
 
 """
 from y2022.days.day01 import d1parse, d1p1, d1p2, d1p2_old
+from y2022.days.day02 import d2p1, d2p2, d2parse
---
+++ y2022/days/day02.py
@@ -0,0 +1,25 @@
+"""
+aoc y2022 day 02
+https://adventofcode.com/2022/day/2
+"""
+
+
+def d2parse(data):
+    """
+    parse
+    """
+    return data
+
+
+def d2p1(data):
+    """
+    part 1
+    """
+    pass
+
+
+def d2p2(data):
+    """
+    part 2
+    """
+    pass
