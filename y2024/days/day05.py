9ddb2c381f7f7383e9be95982791f2afc868d4cb
CGL <charles.giry.laterriere@gmail.com>
2024-12-04 14:30:15 +0100

    prepare y2024 day 05

---
+++ y2024/days/day05.py
@@ -0,0 +1,25 @@
+"""
+aoc y2024 day 05
+https://adventofcode.com/2024/day/5
+"""
+
+
+def d5parse(data):
+    """
+    parse
+    """
+    return data
+
+
+def d5p1(data):
+    """
+    part 1
+    """
+    pass
+
+
+def d5p2(data):
+    """
+    part 2
+    """
+    pass
