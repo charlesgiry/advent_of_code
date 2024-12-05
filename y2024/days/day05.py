6c6ae84f7d8af2796b070ec319f29ecbd1a2b25b
CGL <charles.giry.laterriere@gmail.com>
2024-12-05 21:57:58 +0100

    solutions for y2024 d05

--- y2024/days/day05.py
+++ y2024/days/day05.py
@@ -14,16 +14,13 @@ def d5parse(data):
     for line in data:
         if line == '':
             is_rules = False
-
         elif is_rules:
             splt = line.split('|')
-            is_updated = int(splt[1])
-            needs_update = int(splt[0])
-            if is_updated not in rules:
-                rules[is_updated] = []
-
-            rules[is_updated].append(needs_update)
-
+            before = int(splt[0])
+            after = int(splt[1])
+            if after not in rules:
+                rules[after] = set()
+            rules[after].add(before)
         else:
             splt = line.split(',')
             updates.append([int(i) for i in splt])
@@ -34,15 +31,46 @@ def d5parse(data):
     }
 
 
+# keep incorrect updates from p1 to avoid having to revalidate the entire file
+INCORRECT_UPDATES = []
+
+
 def d5p1(data):
     """
     part 1
     """
-    print(data)
+    rules = data['rules']
+    updates = data['updates']
+    result = 0
+    for update in updates:
+        l = len(update)
+        for i in range(l):
+            if update[i] in rules and any(val in rules[update[i]] for val in update[i + 1:]):
+                INCORRECT_UPDATES.append(update)
+                break
+        else:
+            result += update[l // 2]
+    return result
 
 
 def d5p2(data):
     """
     part 2
+    extremely inefficient solution, but will do until i find something better
     """
-    pass
+    result = 0
+    rules = data['rules']
+    for update in INCORRECT_UPDATES:
+        l = len(update)
+        i = 0
+        while i < l:
+            start = i
+            cont = True
+            while update[start] in rules and any(val in rules[update[start]] for val in update[start+1:]):
+                update[start], update[start+1] = update[start+1], update[start]
+                start += 1
+                cont = False
+            if cont:
+                i += 1
+        result += update[l // 2]
+    return result
